f2153fdc8417a24812fa72c9eac6407b4b4470d8
suryapavan9166 <suryapavan@peoplelinkvc.com>
2022-06-01 14:47:38 +0530

    first sdk

---
+++ suryaSDK/src/main/java/com/peoplelink/suryasdk/MyActivity.java
@@ -0,0 +1,11 @@
+package com.peoplelink.suryasdk;
+
+public class MyActivity {
+
+    public int Add(int a, int b){
+        return a+b;
+    }
+    public int Multi(int a, int b){
+        return a*b;
+    }
+}
